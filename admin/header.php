efe88ce9f968c1e8f0291ffdb4038a5671cac5b4
PriyanshuBhadauriya <62997069+PriyanshuBhadauriya@users.noreply.github.com>
2021-04-15 22:36:17 +0530

    Add files via upload

---
+++ admin/header.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width , initial-scale=1.0">
+	<title>Document</title>
+
+	<!-- bootstrap css-->
+	<link rel="stylesheet"  href="../css/bootstrap.min.css">
+	<!--font awesome css-->
+	<link rel="stylesheet" type="text/css" href="../css/all.min.css">
+	<!-- custom css-->
+	<link rel="stylesheet" type="text/css" href="../css/adminstyle.css">
+</head>
+<body>
+	<nav class="navbar navbar-dark fixed-top p-0 shadow" style="background-color: #225470">
+		<a class="navbar-brand col-sm-3 col-md-2 mr-0" href="adminDashboard.php">E-Learning <small> Admin Area</small></a>
+	</nav>
+	<!--side bar-->
+	<div class="container-fluid mb-5" style="margin-top:40px;">
+		<div class="row">
+			<nav class="col-sm-3 col-md-2 bg-light sidebar py-5 d-print-none">
+				<div class="sidebar-sticky">
+					<ul class="nav flex-column">
+						<li class="nav-link">
+							<a  class="nav-link" href="adminDashboard.php">
+								<i class="fas fa-tachometer-alt"></i>Dashboard
+							</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="courses.php">
+								<i class="fab fa-accessible-icon"></i>Courses
+							</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="lession.php">
+								<i class="fab fa-accessible-icon"></i>Lessons
+							</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="students.php">
+								<i class="fab fa-users"></i>Students
+							</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">
+								<i class="fab fa-table"></i>Sell Report
+							</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">
+								<i class="fab fa-table"></i>Payment Status
+							</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">
+								<i class="fab fa-accessible-icon"></i>Feedback
+							</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">
+								<i class="fab fa-key"></i>Change Password
+							</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="../logout.php">
+								<i class="fab fa-sign-out-alt"></i>Logout
+							</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
